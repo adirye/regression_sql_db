a9ea6acd465ff8efb712c8c6efe5420c85cec3d3
adirye <34780785+adirye@users.noreply.github.com>
2017-12-23 06:53:50 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,98 @@
+#!/usr/bin/python
+
+import sqlite3
+from sqlite3 import Error
+
+def create_connection(db_file):
+    """ create a database connection to the SQLite database
+        specified by the db_file
+    :param db_file: database file
+    :return: Connection object or None
+    """
+    try:
+        print (db_file)
+        conn = sqlite3.connect(db_file)
+        return conn
+    except Error as e:
+        print(e)
+
+    return None
+
+def create_table(conn, create_table_sql):
+    """ create a table from the create_table_sql statement
+    :param conn: Connection object
+    :param create_table_sql: a CREATE TABLE statement
+    :return:
+    """
+    try:
+        c = conn.cursor()
+        c.execute(create_table_sql)
+    except Error as e:
+        print(e)
+
+def select_all_tasks(conn):
+    """
+    Query all rows in the tasks table
+    :param conn: the Connection object
+    :return:
+    """
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM tasks")
+
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+
+
+def select_task_by_priority(conn, priority):
+    """
+    Query tasks by priority
+    :param conn: the Connection object
+    :param priority:
+    :return:
+    """
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM tasks WHERE priority=?", (priority,))
+
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+
+
+def main():
+    database = "C:/Users/adiry/PycharmProjects/regression_sql_db/database.sql"
+
+    # create a database connection
+    conn = create_connection(database)
+
+    sql_create_projects_table = """ CREATE TABLE IF NOT EXISTS projects (
+                                        id integer PRIMARY KEY,
+                                        name text NOT NULL,
+                                        begin_date text,
+                                        end_date text
+                                    ); """
+
+    sql_create_tasks_table = """CREATE TABLE IF NOT EXISTS tasks (
+                                    id integer PRIMARY KEY,
+                                    name text NOT NULL,
+                                    priority integer,
+                                    status_id integer NOT NULL,
+                                    project_id integer NOT NULL,
+                                    begin_date text NOT NULL,
+                                    end_date text NOT NULL,
+                                    FOREIGN KEY (project_id) REFERENCES projects (id)
+                                );"""
+    if conn is not None:
+        # create projects table
+        create_table(conn, sql_create_projects_table)
+        print ("create_table sql_create_projects_table success")
+        # create tasks table
+        create_table(conn, sql_create_tasks_table)
+        print ("create_table sql_create_tasks_table success")
+    else:
+        print("Error! cannot create the database connection.")
+
+if __name__ == '__main__':
+    main()
